e0d7d23e1ffd826fc0cc7cbebde0cf0f28e973ba
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-04-01 15:21:42 +0100

    Disassembler: support 68020 addressing modes in calc_relative_address()

--- tools/hrdb/models/disassembler.cpp
+++ tools/hrdb/models/disassembler.cpp
@@ -88,11 +88,21 @@ static bool calc_relative_address(const operand& op, uint32_t inst_address, uint
         target_address = inst_address + op.pc_disp_index.inst_disp;
         return true;
     }
-    if (op.type == RELATIVE_BRANCH)
+    else if (op.type == RELATIVE_BRANCH)
     {
         target_address = inst_address + op.relative_branch.inst_disp;
         return true;
     }
+    else if (op.type == INDIRECT_POSTINDEXED || op.type == INDIRECT_PREINDEXED ||
+             op.type == MEMORY_INDIRECT || op.type == NO_MEMORY_INDIRECT)
+    {
+        if (op.indirect_index_68020.base_register == INDEX_REG_PC)
+        {
+            target_address = inst_address + op.indirect_index_68020.base_displacement;
+            return true;
+        }
+    }
+
     return false;
 }
 
