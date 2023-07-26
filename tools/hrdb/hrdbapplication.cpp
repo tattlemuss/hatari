7d45ee0327ab0e8ce1ddc3a9c93a2f5a650487d4
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-07-26 21:07:53 +0100

    Add a window icon.

--- tools/hrdb/hrdbapplication.cpp
+++ tools/hrdb/hrdbapplication.cpp
@@ -1,6 +1,10 @@
 #include "hrdbapplication.h"
+#include <QPixmap>
+#include <QIcon>
 
 HrdbApplication::HrdbApplication(int &argc, char **argv) :
     QApplication(argc, argv)
 {
+    QPixmap iconPixmap(":/images/hrdb_icon.png");
+    setWindowIcon(QIcon(iconPixmap));
 }
