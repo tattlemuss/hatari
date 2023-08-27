e4c6e53ef1179a97ac7b7e6119f6f8623204b95a
Steven Tattersall <2294514+tattlemuss@users.noreply.github.com>
2023-08-27 14:40:07 +0100

    Update version string and release notes.

--- tools/hrdb/models/session.h
+++ tools/hrdb/models/session.h
@@ -13,7 +13,7 @@ class Dispatcher;
 class TargetModel;
 class FileWatcher;
 
-#define VERSION_STRING      "0.007 (August 2022)"
+#define VERSION_STRING      "0.008 (August 2023)"
 
 // Shared runtime data about the debugging session used by multiple UI components
 // This data isn't persisted over runs (that is saved in Settings)
