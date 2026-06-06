2a18c3f993e4f46e5d990f8e7885f7bc63693d66
Steven Tattersall <tattlemuss@github.com>
2026-06-06 15:15:04 +0100

    Force Fusion style on Windows to work better with dark/light themes.
    (Untested yet)

--- tools/hrdb/main.cpp
+++ tools/hrdb/main.cpp
@@ -2,6 +2,7 @@
 
 #include <QApplication>
 #include <QSettings>
+#include <QStyleFactory>
 #include "hrdbapplication.h"
 #include <QCommandLineParser>
 
@@ -12,6 +13,12 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("hrdb");
     QCoreApplication::setApplicationVersion(VERSION_STRING);
     QSettings::setDefaultFormat(QSettings::Format::IniFormat);
+
+    // The default style on Windows is based on Vista and does not work well with
+    // light/dark themes, so force the app to the Fusion style, which does.
+#ifdef Q_OS_WINDOWS
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
+#endif
 
     // This creates the app and the session (including loading session settings)
     HrdbApplication app(argc, argv);
